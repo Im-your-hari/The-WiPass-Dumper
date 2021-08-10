d18cdcdae21d0765cd7e40da568b95e197037ff2
Hari <baluoger91@gmail.com>
2021-08-10 22:11:27 +0530

    Everything is set

--- main.py
+++ main.py
@@ -7,54 +7,48 @@ import tkinter as tk
 a = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
 
 profiles = [i.split(":")[1][1:-1] for i in a if "All User Profile" in i]
-print(profiles)
-for i in profiles:
-	#print(i)
-	try:
-		results = subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear',]).decode('utf-8').split('\n')
-		results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-		#print(results)
-	except:
-		pass
+#print(profiles)
 
-'''
-	try:
-		print("{:<30}	{:<}".format(i,results[0]))
-	except IndexError:
-		print("{:<30}	{:<}".format(i,""))
-'''
 j = 0
 #________________________________GUI_________________________________________________________
 root = tk.Tk()
 
 root.title('The-WiPass-Dumper v1.0.0')
-root.config(bg='#1097eb')
+root.config(bg='#000000')
+root.geometry('550x550')
+root.iconphoto(
+	False,
+	tk.PhotoImage(file="./wifi_icon.png")
+)
 
 for i in profiles:
-	#print(i)
 	try:
 		results = subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear',]).decode('utf-8').split('\n')
 		results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-		#print(results)
 	except:
 		pass
 
 
 	try:
-		#print("{:<30}	{:<}".format(i,results[0]))
-		l = Label(root,text="{:<30}	{:<}".format(i,results[0])).place(x=10,y=10+(30*j))
+		l = Label(
+			root,
+			text="{:<30}	{:<}".format(i,results[0]),
+			font=("arial",12,"bold"),
+			bg="gray",
+			justify=CENTER
+		).place(x=10,y=10+(30*j))
 		j+=1
 		pass
 	except IndexError:
-		#print("{:<30}	{:<}".format(i,""))
-		l = Label(root,text="{:<30}	{:<}".format(i,"Bad SSID Format")).place(x=10,y=10+(30*j))
+		l = Label(
+			root,
+			text="{:<30}	{:<}".format(i,"Bad SSID Format"),
+			font=("arial",12,"bold"),
+			bg="gray",
+			justify=CENTER
+		).place(x=10,y=10+(30*j))
 		j+=1
 		pass
-
-
-
-
-
 
 
 root.mainloop()
